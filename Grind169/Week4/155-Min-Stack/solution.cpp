d3b1d9daa02ea270ce71104f591ea0f57918248a
echo <ryne91009@gmail.com>
2025-06-28 16:39:23 +0800

    155

---
+++ Grind169/Week4/155-Min-Stack/solution.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+
+using namespace std;
+
+class MinStack
+{
+
+private:
+    vector<vector<int>> st;
+
+public:
+    MinStack()
+    {
+    }
+    void push(int val)
+    {
+        int minVal = getMin();
+        if (st.empty() || val < minVal)
+        {
+            minVal = val;
+        }
+        st.push_back({val, minVal});
+    }
+
+    void pop()
+    {
+        st.pop_back();
+    }
+
+    int top()
+    {
+        return st.empty() ? -1 : st.back()[0];
+    }
+
+    int getMin()
+    {
+        return st.empty() ? -1 : st.back()[1];
+    }
+};
